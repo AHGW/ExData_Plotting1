06f2950a4cc92f2383914f66c9dd53589738694b
AHGW <addishiwotg@gmail.com>
2015-10-11 15:56:16 -0500

    correcting plot3 legend

--- plot3.R
+++ plot3.R
@@ -17,6 +17,6 @@ with(DT, plot(DateTime, Sub_metering_1,main = " ", type = "n",xlab=" ", ylab= "E
 with(DT, lines(DateTime, Sub_metering_1))
 with(DT, lines(DateTime, Sub_metering_2,col="red"))
 with(DT, lines(DateTime, Sub_metering_3,col="blue"))
-legend("topright", legend = c("Sub_metering_1","Sub_metering_1","Sub_metering_1"),lty=1,col = c("black", "red","blue"))
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col = c("black", "red","blue"))
 
 dev.off()
